cf523fbf0c63b9b8052de69b8305fe8947321fe6
ifranji@gmail.com <ifranji@gmail.com>
2024-11-27 15:49:24 +0100

    Components and props

--- my-app/eslint.config.js
+++ my-app/eslint.config.js
@@ -3,11 +3,14 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from "eslint-config-prettier"
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
+import { EndOfLineState } from 'typescript'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier, eslintPluginPrettier],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,6 +26,12 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "prettier/prettier": [
+        "warn",
+        {
+          endOfLine:"auto"
+        }
+      ]
     },
   },
 )
