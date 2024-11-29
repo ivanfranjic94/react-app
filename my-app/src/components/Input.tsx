a237b528649fa2b4e99a8ed7d9e45a3a72e6573e
ifranji@gmail.com <ifranji@gmail.com>
2024-11-29 14:44:39 +0100

    Error handling 2

--- my-app/src/components/Input.tsx
+++ my-app/src/components/Input.tsx
@@ -4,13 +4,15 @@ import "./Input.css";
 type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   name: string;
   label: string;
-}
+  error?: string;
+};
 
-export default function Input({ name, label, ...props }: InputProps) {
+export default function Input({ name, label, error, ...props }: InputProps) {
   return (
     <div className="Field">
       <label htmlFor={name}>{label}</label>
-      <input id={name} name={name} {...props}/> {/* name parametar je zbog html-a koji ce popuniti form */}
+      <input id={name} name={name} {...props} /> {/* name parametar je zbog html-a koji ce popuniti form */}
+      {error && <span style={{ color: "red" }}>{error}</span>}
     </div>
   );
 }
--- my-app/src/feature/course-apply/Apply.tsx
+++ my-app/src/feature/course-apply/Apply.tsx
@@ -4,10 +4,15 @@ import CustomButton from "../../components/CustomButton";
 import Input from "../../components/Input";
 import TextArea from "../../components/TextArea";
 import Title from "../../components/Title";
+import { useState } from "react";
+import { AttendantsRequest } from "../../model/attendants";
 
 export default function Apply() {
+  // prvo definicije hookova, konstanti, funkcija pa return
   const params = useParams(); //koristimo jer cemo iz urla citati putanju u kojoj ce biti id coursea, a on treba da bi api prihvatio post request
   const navigate = useNavigate();
+  const [errors, setErrors] = useState<Record<keyof AttendantsRequest, string>>();
+
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
@@ -16,9 +21,27 @@ export default function Apply() {
       data[name] = value;
     });
 
+    const formErrors = {};
+    if (!data.firstName) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      formErrors.firstName = "Fali first name";
+    }
+    if (!data.lastName) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      formErrors.lastName = "Fali last name";
+    }
+    if (Object.keys(formErrors).length > 0) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      setErrors(formErrors);
+      return;
+    }
+
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    applyAttendant({ ...data, courseId: params.id! }) //! jer znam da ce to biti popunjeno
+    applyAttendant({ ...data, courseId: params.id! }) //   ! jer znam da ce to biti popunjeno
       .then(() => navigate("/courses"));
     //.catch;
   };
@@ -26,8 +49,8 @@ export default function Apply() {
     <div>
       <Title title="Apply to courses" />
       <form onSubmit={onSubmit}>
-        <Input name="firstName" label="First name" required={true} />
-        <Input name="lastName" label="Last name" />
+        <Input name="firstName" label="First name" error={errors?.firstName} /> {/* moze se dodati required true*/}
+        <Input name="lastName" label="Last name" error={errors?.lastName} />
         <Input name="email" label="Email" />
         <Input name="company" label="Company" />
         <TextArea name="message" label="Message" />
